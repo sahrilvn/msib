c3824947e05afc77aa80d65bc0f1d9df601259cb
Sahril Angga Permana <43839821+sahrilvn@users.noreply.github.com>
2022-11-01 01:39:58 +0700

    Create numbers.kt

---
+++ numbers.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val a: int = 10000
+    val d: double = 100.00
+    val f: float = 100.00f
+    val l: long = 10000000004L
+    val s: short = 10
+    val b: byte = byte.MAX_VALUE
+
+    println("Int Value is" + a)
+    println("Double Value is" + d)
+    println("Float Value is" + f)
+    println("Long Value is" + l)
+    println("Short Value is" + s)
+    println("Byte Value is" + b)
+}
