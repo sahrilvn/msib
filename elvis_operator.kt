6bbc6870d635dfdc813bbc172ef14c0814dc8a13
Sahril Angga Permana <43839821+sahrilvn@users.noreply.github.com>
2022-11-01 03:23:58 +0700

    Create elvis_operator.kt

---
+++ elvis_operator.kt
@@ -0,0 +1,11 @@
+fun main(){
+   val dateOut: String? = null
+   var status = "Booked"
+
+   status = "Checkout"
+
+   if(status.equals("Checkout")) {
+    dateOut= "28/10/2022 12:05:00"
+   }
+   println(dateOut)
+}
